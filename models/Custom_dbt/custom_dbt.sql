6c1865cd84f635e358a5cbfe150a8b35dfaab605
Nanduzz99 <prmakki@gmail.com>
2022-03-08 20:43:21 +0530

    sqlcorection1

--- models/Custom_dbt/custom_dbt.sql
+++ models/Custom_dbt/custom_dbt.sql
@@ -1,6 +1,6 @@
 select
     a.brand_name,
-    b.display_name,
+    b.display_name
 from {{ ref('custom_ra_catalog_brand') }} a
 left join {{ ref('custom_ra_catalog_category') }} b
 on a._airbyte_custom_ra_catalog_brand_hashid = b._airbyte_custom_ra_catalog_category_hashid
