c45be70d4483c6251502ab27cb9a36d6a872d051
Nanduzz99 <prmakki@gmail.com>
2022-03-08 20:21:05 +0530

    custom_dbt

--- models/Custom_dbt/custom_dbt.sql
+++ models/Custom_dbt/custom_dbt.sql
@@ -3,4 +3,4 @@ select
     b.display_name,
 from {{ ref('custom_ra_catalog_brand') }} a
 left join {{ ref('custom_ra_catalog_category') }} b
-no _airbyte_custom_ra_catalog_category_hashid
+no a._airbyte_custom_ra_catalog_brand_hashid = b._airbyte_custom_ra_catalog_category_hashid
